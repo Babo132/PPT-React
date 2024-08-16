df96e47b45ce20d3ee16d19fe9447c0c212d8361
Babo132 <fajardosebas132@gmail.com>
2024-08-16 13:45:14 -0600

    first commit

---
+++ src/context/SocketContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+import { useSocket } from "../Hooks/useSocket";
+
+export const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+  const { Online, socket } = useSocket("http://localhost:3300");
+  return (
+    <>
+      <SocketContext.Provider value={{ socket, Online }}>
+        {children}
+      </SocketContext.Provider>
+    </>
+  );
+};
